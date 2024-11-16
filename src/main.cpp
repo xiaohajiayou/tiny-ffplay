2a809c3f19a72a2b63cbda146113b3219a80bd04
xiaohajiayou <923390377@qq.com>
2024-11-17 00:17:31 +0800

    Update: 配置基础编译开发环境

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+
+int main(){
+
+    int a = 0;
+    int b = 0;
+    std::cout<<"Hello World!"<<std::endl;
+    std::cout<<"Hello World!"<<std::endl;
+    
+    return 0;
+}
